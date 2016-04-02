7ff623a5ee727fde7da670f3d18774b18eef51b6
vinivendra <vinivendra@gmail.com>
2016-04-02 13:00:12 -0400

    Added comments for player code example.

---
+++ Loop/Loop/PlayerHandler.swift
@@ -0,0 +1,30 @@
+
+import EZAudio
+
+//self.player = [EZAudioPlayer audioPlayerWithDelegate:self];
+//self.player.shouldLoop = YES;
+
+//-(void)play:(id)sender
+//{
+//	if (![self.player isPlaying])
+//	{
+//		if (self.player.state == EZAudioPlayerStateEndOfFile)
+//		{
+//			[self.player seekToFrame:0];
+//		}
+//		if (self.audioPlot.plotType == EZPlotTypeBuffer
+//			&& self.audioPlot.shouldFill == YES)
+//		{
+//			self.audioPlot.plotType = EZPlotTypeRolling;
+//		}
+//		[self.player play];
+//	}
+//	else
+//	{
+//		[self.player pause];
+//	}
+//}
+
+//[self.player seekToFrame:(SInt64)value];
+
+//self.player.audioFile = [EZAudioFile audioFileWithURL:filePathURL];
