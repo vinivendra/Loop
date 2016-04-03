28886f5a7724b44e4b7d01df17f32aae690055d0
vinivendra <vinivendra@gmail.com>
2016-04-03 00:37:49 -0400

    Added TODOs

--- Loop/Loop/AppDelegate.swift
+++ Loop/Loop/AppDelegate.swift
@@ -1,18 +1,17 @@
 import Cocoa
 
+// TODO: Add a reset button to delete recordings and start app anew
+// TODO: Test with real guitar/music to fix bugs
+// TODO: Add mouse support
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
 
-
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
-		// Insert code here to initialize your application
-	}
-
-	func applicationWillTerminate(aNotification: NSNotification) {
-		// Insert code here to tear down your application
-	}
-
-
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
 }
